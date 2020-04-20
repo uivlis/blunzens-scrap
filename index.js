e4c50965883ae6dd08456a03148ce9a1a7b4e8f7
uivlis <vladsilviufarcas@gmail.com>
2020-04-20 12:32:17 +0300

    First phase of scrapper

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const { Client } = require('@conversationai/perspectiveapi-js-client');
-var osmosis = require('osmosis');
+const osmosis = require('osmosis');
+const walker = require('puppeteer-walker')();
+const puppeteer = require('puppeteer');
 const { bluzelle } = require('./blzjs/src/main.js');
 const Hashes = require('jshashes');
+const googleTrends = require('google-trends-api');
+const qs = require('querystringify');
+const chrono = require('chrono-node');
+
+require('events').EventEmitter.prototype._maxListeners = 1000;
+require('events').defaultMaxListeners = 1000;
 
 require('dotenv').config();
 
@@ -10,6 +18,10 @@ const perspective = new Client(process.env.PERSPECTIVE_API_KEY);
 const config = require('./blz-config.js');
 
 const main = async () => {
+
+    let buzz = [];
+
+    console.log();
 
     let blz;
 
@@ -25,35 +37,61 @@ const main = async () => {
         console.error(e.message);
     }
 
-    // add scrapper
+    googleTrends.realTimeTrends({
+        geo: 'US'
+    }).then(res => {
+        JSON.parse(res).storySummaries.trendingStories.map(story => {
+            story.entityNames.map(async entity => {
 
-    const text = "something inflamatory";
+                osmosis
+                    .get('https://duckduckgo.com/html/' + qs.stringify({q: entity}, true))
+                    // .click('[data-value="-2"]')
+                    .find('.result__body')
+                    .set({
+                        snippets: '.result__snippet',
+                        tempLink: '.result__url',
+                        title: '.result__title',
+                    }).data(function(d) {
+                        console.log("\n\n" + d.snippets);
+                        console.log("\n\n" + d.tempLink);
+                        console.log("\n\n" + d.title);
+                    }).error(console.error)
+                    .log(console.log)
+                    .debug(console.debug);
 
-    const analysis = await perspective.getScores(text);
-
-    const speech = JSON.stringify({
-        author: "somebody",
-        text: text,
-        platform: "Twitter",
-        tempLink: "url/path/to/text",
-        date: "03/20/2020",
-        censorability: ((analysis.TOXICITY - analysis.SPAM) + 1) / 2.0
-    })
-
-    const id = new Hashes.SHA256().hex(speech);
-
-    try {
-        let res = await blz.create(id, speech, config.gas_params);
-        successPrint(res);
-        res = await blz.read(id);
-        successPrint(JSON.parse(res));
-    } catch(e) {
+            })
+        })
+    }).catch(e => {
         console.error(e.message);
-    };
+    });
+
+    // const text = "something inflamatory";
+
+    // const analysis = await perspective.getScores(text);
+
+    // const speech = JSON.stringify({
+    //     author: "somebody",
+    //     text: text,
+    //     platform: "Twitter",
+    //     tempLink: "url/path/to/text",
+    //     date: "03/20/2020",
+    //     censorability: ((analysis.TOXICITY - analysis.SPAM) + 1) / 2.0
+    // })
+
+    // const id = new Hashes.SHA256().hex(speech);
+
+    // try {
+    //     let res = await blz.create(id, speech, config.gas_params);
+    //     successPrint(res);
+    //     res = await blz.read(id);
+    //     successPrint(JSON.parse(res));
+    // } catch(e) {
+    //     console.error(e.message);
+    // };
 }
 
 main();
 
 function successPrint(res) {
-    console.log(typeof res != 'undefined' ? res : "success");
+    console.log(typeof res != 'undefined' ? res : 'success');
 }
